4f10cd918d1328ce04f90f9cc55b91e2def4f36f
samrat53 <samrat05hccs@gmail.com>
2024-01-19 19:31:34 +0530

    built regulations page

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,57 +3,70 @@ import React from "react";
 const Faq = () => {
   const faqData = [
     {
-      question: "How can I join the 'Introduction to Web Development' session?",
-      answer:
-        "Simply attend the session at the specified time. No prior registration is required.",
+      question: "Prestigious Title at Stake - 'The Best School of Tripura':",
+      answer: [
+        "The quizzing event is not just a competition; it's a quest for excellence. The winning school will be bestowed with the esteemed title of 'The Best School of Tripura in the field of Biology Education,' adding a layer of significance to the intellectual pursuit.",
+      ],
     },
     {
-      question: "Is the '21 Days Challenge' suitable for beginners?",
-      answer:
-        "Absolutely! The challenge is designed for both beginners and experienced coders to enhance their web development skills.",
+      question: "Diverse Team Composition Emphasizing Inclusivity:",
+      answer: [
+        "a. Teams will consist of six students each, ensuring representation from classes 7 to 12.",
+        "b. Recognizing logistical constraints, if all specified classes aren't available, a minimum of four classes should be represented in a team, promoting flexibility and inclusivity.",
+      ],
     },
     {
-      question:
-        "Can I use platforms other than Replit or Codepen for the 21 Days Challenge?",
-      answer:
-        "No, submissions from other platforms won't be accepted. Stick to Replit, Codepen, or local coding with Google Drive uploads.",
+      question: "Ample Participation Encouraged:",
+      answer: [
+        "To enrich the competitive landscape, each school is encouraged to field a minimum of five teams, fostering a spirit of healthy competition among institutions.",
+      ],
     },
     {
-      question: "Is collaboration allowed during the 21 Days Challenge?",
-      answer:
-        "Collaboration is encouraged, but sharing solutions publicly during the task period is strictly prohibited.",
+      question: "Equitable Evaluation with Percentage Conversion:",
+      answer: [
+        "To address potential disparities in the number of participating teams from different schools, raw scores will be converted to percentages, ensuring a fair assessment across the board.",
+      ],
     },
     {
-      question: "What happens if I miss a day in the 21 Days Challenge?",
-      answer:
-        "Try to complete each task daily, but if you miss a day, catch up on the following day. Late submissions, however, might not be accepted, and any points won’t be provided for late submission.",
+      question: "Individual Team Recognition:",
+      answer: [
+        "Teams will be evaluated individually, allowing for nuanced acknowledgment of each team's efforts and accomplishments, fostering a sense of achievement among participants.",
+      ],
     },
     {
-      question:
-        "Can I share my solutions on GitHub during the 21 Days Challenge?",
-      answer:
-        "Yes, but only after the specified task period has ended. Respect the challenge rules.",
+      question: "Strategic Round-wise Cut-off Percentages:",
+      answer: [
+        "a. Each round will feature a minimum cut-off percentage, adding a strategic element to the competition.",
+        "b. In cases where teams achieve a perfect score, the top-performing teams will progress to subsequent rounds, injecting an element of competition even within the highest-scoring teams.",
+      ],
+    },
+    {
+      question: "Excellence Rewarded - Prizes for the Pinnacle Performers:",
+      answer: [
+        "The team with the highest cumulative score will not only earn bragging rights but will also be rewarded with prizes, celebrating and encouraging excellence in biological knowledge.",
+      ],
+    },
+    {
+      question: "Special Provisions for Schools Without Classes 11 and 12:",
+      answer: [
+        "a. Acknowledging variations in school structures, schools without classes 11 and 12 will be given special consideration.",
+        "b. These schools can nominate two additional students from any other class, ensuring that every institution, regardless of its grade structure, has an equal opportunity to participate.",
+      ],
     },
   ];
 
   const FaqItem = ({ question, answer }) => {
     return (
-      <div className="question-box px-[3rem] py-5 mx-[7rem] text-xl">
-        {/* <div className="chat chat-start ">
-          <div className="chat-bubble chat-bubble-primary">
-          {`Q. ${question}`}
-          </div>
-        </div>
-        <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-accent mt-6">{`Ans. ${answer}`}</div>
-        </div> */}
-        <div className="collapse collapse-plus bg-base-200">
+      <div className="question-box py-5 mx-[3em] text-xl ">
+        <div className="collapse bg-base-300 collapse-open ">
           <input type="radio" name="my-accordion-3" checked="checked" />
-          <div className="collapse-title text-xl font-medium">
-            {`Q. ${question}`}
+          <div className="collapse-title text-xl font-medium text-center underline">
+            {`${question}`}
           </div>
           <div className="collapse-content">
-            <p>{`Ans. ${answer}`}</p>
+            {answer.map((item) => (
+              <p>{item}</p>
+            ))}
           </div>
         </div>
       </div>
@@ -62,13 +75,28 @@ const Faq = () => {
 
   return (
     <>
-      <h1 className="px-[5rem] pt-[3.5rem] flex flex-col items-center topic-class text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-white">
+      <h1 className="px-[5rem] py-[3.5rem] flex flex-col items-center topic-class text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-teal-300">
         Rules for the Inter School Biology Quizzing Event <br />
         <span className="underline">“Bio-Chase 1.0”</span>
       </h1>
       {faqData.map((item, index) => (
         <FaqItem key={index} question={item.question} answer={item.answer} />
       ))}
+
+      <div className="question-box py-5 mx-[3em] text-xl">
+        <div className="collapse collapse-open ">
+          <div className="collapse-content">
+            <h3 className="text-white text-center">
+              The event is not just a competition; it's a celebration of
+              knowledge and a testament to the diversity of learning
+              environments across schools in Tripura.
+              <br/>
+              <br/>
+              <span className="text-2xl font-bold">All the Best!!!</span>
+            </h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
           <p>Copyright © 2024 - All right reserved</p>
         </aside>
         <nav>
-          <div className="grid grid-flow-col gap-4">
+          {/* <div className="grid grid-flow-col gap-4">
             <a>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +61,7 @@ const Footer = () => {
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
             </a>
-          </div>
+          </div> */}
         </nav>
       </footer>
     </div>
